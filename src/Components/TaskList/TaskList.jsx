5d31e5c611a4baf3587ce23447e1b65cd2fb554a
Sagitario-lab <ignacioulman@hotmail.com>
2023-11-17 17:01:03 -0300

    asd

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -3,20 +3,21 @@ import {Box, Button, Card, CardContent} from "@mui/material";
 
 export const TaskList = ({ taskList, setTaskList }) => {
 
-    const filter = (index) => {
-        const list = taskList.filter((_, idx) => idx !== index);
-        setTaskList(list);
-    }
-
     return (
     <Box display={'flex'} gap={5} justifyContent={'center'} flexWrap={'wrap'}>
         {taskList.map((data, idx) => (
-        <Card key={idx} variant="outlined">
-            <CardContent>
-                <TaskItem name={data.name} description={data.description} />
-                <Button onClick={() => filter(idx)}>Eliminar</Button>
-            </CardContent>
-        </Card>
+            <Card key={idx} variant="outlined">
+                <CardContent>
+                    <TaskItem
+                        name={data.name}
+                        description={data.description}
+                        setTaskList={setTaskList}
+                        taskList={taskList}
+                        index={idx}
+                        state={data.state}
+                    />
+                </CardContent>
+            </Card>
         ))}
     </Box>
     );
